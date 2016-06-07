3d7c47d94f7259acea6938ed393909de759ea521
mtaner <mervetaner88@gmail.com>
2016-06-07 11:56:53 +0100

    Added params to the 'named-cat'

--- app.rb
+++ app.rb
@@ -4,6 +4,15 @@ get "/" do
 	"Hello!"
 end
 
-get "/cat" do
+get "/random-cat" do
+	@name = ["Amigo", "Oscar", "Viking"].sample
+	@age = rand(5)
+	erb(:index)
+end
+
+get "/named-cat" do
+	p params
+	@name = params[:name]
+	@age = params[:age]
 	erb(:index)
 end
